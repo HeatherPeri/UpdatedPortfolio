9d2d210cf4ff6e705421bfd96e252336d87c3538
Heather Peri <heatherhperi@gmail.com>
2019-08-01 15:40:43 -0400

    adding my works to the examples page

--- hpportfolio/src/components/Examples.js
+++ hpportfolio/src/components/Examples.js
@@ -17,10 +17,14 @@ function Examples() {
         <Card>
         <CardImg top width="50%" src="https://images.unsplash.com/photo-1564326680392-26c9e5220866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" Style="height:500px" alt="Card image cap" />
         <CardBody className="text-center">
-          <CardTitle>First Project</CardTitle>
-          <CardSubtitle>Info about first project</CardSubtitle>
-          <CardText>more info on the project</CardText>
-          <Button>Github</Button>
+          <CardTitle>Astrono(ME)</CardTitle>
+          <CardSubtitle>Full application created in a team </CardSubtitle>
+          <CardText>In this group based project my controbutions include but are not limited to:
+                    Creating the events page, UI responsiveness</CardText>
+
+          <Button href="https://github.com/HeatherPeri/Project2.git">Github</Button> 
+          <Button href="https://see-the-stars.herokuapp.com/index.html">Deployed Site</Button>
+
         </CardBody>
         </Card>
         </Col>
@@ -29,24 +33,27 @@ function Examples() {
             <Card>
         <CardImg top width="50%" src="https://images.unsplash.com/photo-1564326680392-26c9e5220866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" Style="height:500px" alt="Card image cap" />
         <CardBody className="text-center">
-          <CardTitle>First Project</CardTitle>
-          <CardSubtitle>Info about first project</CardSubtitle>
-          <CardText>more info on the project</CardText>
-          <Button>Github</Button>
+          <CardTitle>Animation</CardTitle>
+          <CardSubtitle>Front end responsiveness</CardSubtitle>
+          <CardText>10 buttons all linked to a different giphy API. Once the buttons are clicked gifs are loaded to page. The gifs respond to click functions to start and stop the animations</CardText>
+          <Button href="https://github.com/HeatherPeri/Animate">Github</Button>
         </CardBody>
         </Card>
         </Col>
     </Row>
-
+<br></br>
     <Row>
         <Col size="md-6">
         <Card>
         <CardImg top width="50%" src="https://images.unsplash.com/photo-1564326680392-26c9e5220866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" Style="height:500px" alt="Card image cap" />
         <CardBody className="text-center">
-          <CardTitle>First Project</CardTitle>
-          <CardSubtitle>Info about first project</CardSubtitle>
-          <CardText>more info on the project</CardText>
-          <Button>Github</Button>
+          <CardTitle>Bamazon</CardTitle>
+          <CardSubtitle>MySQL database </CardSubtitle>
+          <CardText>This application runs for two seperate sides, the consumer and manager. The consumer can see what products are listed, buy in multiples and when the item is purchased the number of items are removed. 
+              In the manager side they can add inventory to existing items or add new items to the list.
+          </CardText>
+          <Button href="https://github.com/HeatherPeri/bamazon">Github</Button>
+
         </CardBody>
         </Card>
         </Col>
@@ -76,7 +83,7 @@ function Examples() {
           <CardTitle>Salon Synchronicity</CardTitle>
           <CardSubtitle>Created full website </CardSubtitle>
           <CardText>more info on the project</CardText>
-          <Button>Go To Site</Button>
+          <Button>Browse Site</Button>
         </CardBody>
         </Card>
         </Col>
@@ -88,7 +95,7 @@ function Examples() {
           <CardTitle>LN Organix</CardTitle>
           <CardSubtitle>Plug-In + Inventory</CardSubtitle>
           <CardText>helped with plug-ins and keeping up with inventory</CardText>
-          <Button>Github</Button>
+          <Button>Browse Site</Button>
         </CardBody>
         </Card>
         </Col>
